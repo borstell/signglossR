7442cef0b0ad5878b80e2f6d3ed97800dedcfca5
Carl Börstell <carlborstell@Carls-MacBook-Pro.local>
2020-04-08 15:56:34 +0200

    New funs

--- R/get_video_asl.R
+++ R/get_video_asl.R
@@ -8,16 +8,12 @@
 #' @export
 get_video_asl <- function(id) {
   if (signglossR::notNumeric(id)) {
-    print("lol")
-    print(id)
     id <- signglossR::gloss2id_asl(id)
   }
   asl_vid <- xml2::read_html(paste0("https://aslsignbank.haskins.yale.edu", "/dictionary/gloss/", id, ".html")) %>%
     rvest::html_nodes("[id='videoplayer']") %>%
     rvest::html_attr("src")
-  print(asl_vid)
   vid_name <- paste0("ASL_", gsub(".*/", "", asl_vid))
-  print(asl_vid)
   utils::download.file(paste0("https://aslsignbank.haskins.yale.edu", asl_vid), destfile = vid_name)
   print(paste0("Downloaded file ", vid_name))
 }

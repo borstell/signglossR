29dacf5fbceab69944d8a384682aeca0ba9c0f40
Carl Börstell <carlborstell@Carls-MacBook-Pro.local>
2020-04-08 08:30:36 +0200

    Corrected fun

--- R/gloss2id_asl.R
+++ R/gloss2id_asl.R
@@ -7,9 +7,14 @@
 #' @return The unique ID number linked to the sign used for indexing online
 #' @export
 gloss2id_asl <- function(gloss) {
+  if(!assertthat::is.string(gloss)) {
+    print("The `gloss` argument has to be a word string (e.g. 'DEAFix')")
+  }
+  else {
   asl_gloss <- xml2::read_html(paste0("https://aslsignbank.haskins.yale.edu/signs/search/?search=%5E", gloss, "%24&keyword=")) %>%
     rvest::html_nodes("*") %>%
     rvest::html_attrs()
-  asl_id <- gsub(".*_", "", as.character(asl_gloss[741]))
-  return(asl_id)
+    asl_id <- gsub(".*_", "", as.character(asl_gloss[741]))
+    return(asl_id)
+  }
 }

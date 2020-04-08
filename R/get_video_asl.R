8686fbab9991f0f6a3fd0b7399143f97ce9f0cd7
Carl Börstell <carlborstell@Carls-MacBook-Pro.local>
2020-04-08 07:42:46 +0200

    More fun

---
+++ R/get_video_asl.R
@@ -0,0 +1,19 @@
+#' Get ASL sign video (ASL Signbank)
+#'
+#' This function inputs an ASL ID gloss or number and downloads the video
+#' of the corresponding sign entry in ASL Signbank.
+#'
+#' @param id ID number for the ASL sign
+#' @return The name of the video file that was downloaded
+#' @export
+get_video_asl <- function(id) {
+  if(!is.na(as.numeric(id))) {
+    id <- signglossR::gloss2id_asl(id)
+  }
+  asl_vid <- xml2::read_html(paste0("https://aslsignbank.haskins.yale.edu/", "/dictionary/gloss/", id, ".html")) %>%
+      rvest::html_nodes("[id='videoplayer']") %>%
+      rvest::html_attr("src")
+  vid_name <- paste0("ASL_", gsub(".*/", "", asl_vid))
+  utils::download.file(paste0("https://aslsignbank.haskins.yale.edu", asl_vid), destfile = vid_name)
+  print(paste0("Downloaded file ", vid_name))
+}

f6ee6b6b61130b6adeb21e419b42230cfcc7b4e8
Carl Börstell <calleborstell@gmail.com>
2021-02-26 02:39:00 +0100

    v2.1.0 aslsignbank correction

--- R/gloss2id_asl.R
+++ R/gloss2id_asl.R
@@ -14,9 +14,9 @@ gloss2id_asl <- function(gloss) {
     asl_gloss <- xml2::read_html(paste0("https://aslsignbank.haskins.yale.edu/signs/search/?search=%5E", gloss, "%24&keyword=")) %>%
       rvest::html_nodes("*") %>%
       rvest::html_attrs()
-    asl_class <- gsub("_.*", "", as.character(asl_gloss[741]))
+    asl_class <- gsub("_.*", "", as.character(asl_gloss[745]))
     if (asl_class == "focusgloss") {
-      asl_id <- gsub(".*_", "", as.character(asl_gloss[741]))
+      asl_id <- gsub(".*_", "", as.character(asl_gloss[745]))
       return(as.character(asl_id))
     }
     else {

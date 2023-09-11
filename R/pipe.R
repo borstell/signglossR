89e0e90f0ff656e9dfa513d9cd69613866eda3c7
Carl Börstell <calleborstell@gmail.com>
2023-09-11 18:38:13 +0200

    Update pipe.R

--- R/pipe.R
+++ R/pipe.R
@@ -1,6 +1,6 @@
 #' Pipe graphics
 #'
-#' Like dplyr, ggvis also uses the pipe function, \code{\%>\%} to turn
+#' signglossR uses the magrittr pipe function, \code{\%>\%} to turn
 #' function composition into a series of imperative statements.
 #'
 #' @importFrom magrittr %>%
